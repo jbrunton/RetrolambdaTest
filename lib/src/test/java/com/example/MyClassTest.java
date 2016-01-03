f1caccd6b50f72e0029a9a5c54f7e45d0c83a3b3
John Brunton <jbrunton@zipcar.com>
2016-01-03 15:32:20 +0000

    add retrolambda and lib project

---
+++ lib/src/test/java/com/example/MyClassTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MyClassTest {
+    @Test public void test() {
+        assertThat(1).isEqualTo(2);
+    }
+}
